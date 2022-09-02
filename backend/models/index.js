5f4f464cec7b257685186d680f174083267e2ed5
Jaytee <Padilla.Jaytee@gmail.com>
2022-09-01 22:15:22 -0700

    Created data association between User and Car models

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,15 @@
 const User = require('./User');
 const Car = require('./Car');
 
+// data associations
+User.hasMany(Car, {
+  foreignKey: 'user_id'
+});
+
+Car.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
 module.exports = {
   User,
   Car
